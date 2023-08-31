24276a0dd3b5cd51974dce6a14e4a018608e4b81
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-30 22:57:37 -0700

    Update DBFuctions.js

--- e-com/src/Component/DBFuctions.js
+++ e-com/src/Component/DBFuctions.js
@@ -2,22 +2,22 @@ const pool = require('./Connection');
 
 function insertData(data) {
     const sql = 'INSERT INTO Product (ProductName, ProductPrice, ProductStock, ProdctDescription, ProductImage_url) VALUES (?, ?, ?, ?, ?)';
-    const values = [data.Name, data.Price, data.Stock, data.Description, data.image]; 
+    const values = [data.Name, data.Price, data.Stock, data.Description, data.image];
     pool.query(sql, values, (error, results) => {
         if (error) {
             console.error('Error inserting data:', error);
-        } 
+        }
         console.log('Data inserted successfully');
     });
-}  
+}
 
-function GetData(){
-    const sql  = 'selct * from Product';
-    pool.query(sql , (error , results)=>{
+function GetData() {
+    const sql = 'SELECT * FROM Product';
+    pool.query(sql, (error, results) => { 
         if (error) {
-         console.log('Error in Geting Data');
+            console.log('Error in Geting Data');
         }
-        console.log('Data is loded sccful');
+        console.log('Data is Loaded  successfully');
     })
 }
 
@@ -29,4 +29,3 @@ function GetData(){
 module.exports = {
     insertData,
 };
- 
